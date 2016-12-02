980f5ae677848a6ea89ba85d07195ea623d2127a
varvelworld <varvelworld@gmail.com>
2016-12-03 02:16:48 +0800

    重构名称及包路径

--- src/main/java/io/github/varvelworld/var/ioc/annotation/IocAnnotation.java
+++ src/main/java/io/github/varvelworld/var/ioc/annotation/IocAnnotation.java
@@ -1,11 +1,12 @@
-package io.github.varvelworld.var.ioc.meta;
+package io.github.varvelworld.var.ioc.annotation;
 
 import io.github.varvelworld.var.ioc.annotation.AnnotationBeansMetaFactoryImpl;
+import io.github.varvelworld.var.ioc.meta.BeansMeta;
 
 /**
  * Created by luzhonghao on 2016/11/26.
  */
-public class IocMeta {
+public class IocAnnotation {
 
     public static BeansMeta beansMetaByAnnotation(Class<?> clazz) {
         return new AnnotationBeansMetaFactoryImpl(clazz).createBeansMeta();
--- src/test/java/io/github/varvelworld/var/ioc/annotation/AnnotationIocTest.java
+++ src/test/java/io/github/varvelworld/var/ioc/annotation/AnnotationIocTest.java
@@ -1,9 +1,11 @@
-package io.github.varvelworld.var.ioc.core;
+package io.github.varvelworld.var.ioc.annotation;
 
 import io.github.varvelworld.var.ioc.HelloPOJO;
 import io.github.varvelworld.var.ioc.HiService;
 import io.github.varvelworld.var.ioc.TestBeans;
-import io.github.varvelworld.var.ioc.meta.IocMeta;
+import io.github.varvelworld.var.ioc.annotation.IocAnnotation;
+import io.github.varvelworld.var.ioc.core.IocContainer;
+import io.github.varvelworld.var.ioc.core.IocContainerImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,7 +17,7 @@ public class AnnotationIocTest {
     @Test
     public void addBeans() throws Exception {
         final IocContainer iocContainer = new IocContainerImpl();
-        iocContainer.addBeans(IocMeta.beansMetaByAnnotation(TestBeans.class));
+        iocContainer.addBeans(IocAnnotation.beansMetaByAnnotation(TestBeans.class));
         HelloPOJO helloPOJO = (HelloPOJO) iocContainer.getBean("fun");
         HelloPOJO helloPOJO2 = (HelloPOJO) iocContainer.getBean("fun2");
         Assert.assertEquals("hello world", helloPOJO.getName());
@@ -25,7 +27,7 @@ public class AnnotationIocTest {
     @Test
     public void injectBean() throws Exception {
         final IocContainer iocContainer = new IocContainerImpl();
-        iocContainer.addBeans(IocMeta.beansMetaByAnnotation(TestBeans.class));
+        iocContainer.addBeans(IocAnnotation.beansMetaByAnnotation(TestBeans.class));
         iocContainer.injectBeans();
         HiService hiService = (HiService) iocContainer.getBean("hi");
         Assert.assertEquals("hello world2", hiService.hello());
